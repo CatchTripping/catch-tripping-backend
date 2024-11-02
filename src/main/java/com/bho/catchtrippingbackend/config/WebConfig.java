9b46f950dfcebdb711b21aa210cac47544e04832
임혁 <cs.javah@kakao.com>
2024-11-02 14:25:42 +0900

    모든 경로 filter 허용

--- src/main/java/com/bho/catchtrippingbackend/config/WebConfig.java
+++ src/main/java/com/bho/catchtrippingbackend/config/WebConfig.java
@@ -1,11 +1,27 @@
 package com.bho.catchtrippingbackend.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        return
+                http
+                        .csrf(AbstractHttpConfigurer::disable)
+                        .authorizeHttpRequests(authorize ->
+                                authorize
+                                        .requestMatchers("/**").permitAll().anyRequest().authenticated()
+                        ).build();
+
+    }
 
     @Override
     public void addCorsMappings(CorsRegistry registry) {
