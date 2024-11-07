108d361a4817023d8f4af1a7d4ca42964efd6910
임혁 <cs.javah@kakao.com>
2024-11-07 17:19:25 +0900

    정리 끝!

--- src/main/java/com/bho/catchtrippingbackend/attractions/dto/AttractionSearchCriteria.java
+++ src/main/java/com/bho/catchtrippingbackend/attractions/dto/AttractionSearchCriteria.java
@@ -1,12 +1,16 @@
 package com.bho.catchtrippingbackend.attractions.dto;
 
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.time.LocalDateTime;
 
 @Data
 @Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class AttractionSearchCriteria {
     private Integer areaCode;
     private Integer sigunguCode;
