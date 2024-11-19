a8b32eb6ad079ccb4b87455ee4bd154e05cd8998
김상욱 <kimsangouk1020@gmail.com>
2024-11-19 17:12:07 +0900

    TravelRecommendation API STOP

---
+++ src/main/java/com/bho/catchtrippingbackend/travelrecommend/dto/reponse/TravelRecommendationResponse.java
@@ -0,0 +1,16 @@
+//package com.bho.catchtrippingbackend.travelrecommend.dto.reponse;
+//
+//import lombok.Data;
+//
+//import java.util.List;
+//
+//@Data
+//public class TravelRecommendationResponse {
+//    private List<Destination> destinations;
+//
+//    @Data
+//    public static class Destination {
+//        private String name;
+//        private String description;
+//    }
+//}
---
+++ src/main/java/com/bho/catchtrippingbackend/travelrecommend/dto/request/TravelRecommendationRequest.java
@@ -0,0 +1,13 @@
+//package com.bho.catchtrippingbackend.travelrecommend.dto.request;
+//
+//import lombok.Data;
+//
+//import java.util.List;
+//
+//@Data
+//public class TravelRecommendationRequest {
+//    private String selectedTravelType;
+//    private String budgetRange;
+//    private List<String> activities;
+//    private String travelDuration;
+//}
